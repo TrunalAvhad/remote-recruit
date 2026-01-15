5a3f2c7714ed781cd4dcbe561100f8807d7e200f
TrunalAvhad <trunalrock2005@gmail.com>
2026-01-15 20:19:28 +0530

    session page build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import ProblemsPage from './pages/ProblemsPage';
 import DashboardPage from './pages/DashboardPage';
 import { Toaster } from 'react-hot-toast';
 import ProblemPage from './pages/ProblemPage';
+import SessionPage from './pages/SessionPage';
 
 function App() {
 
@@ -19,6 +20,8 @@ function App() {
       <Route path="/dashboard" element={isSignedIn ?<DashboardPage /> : <Navigate to="/" />} />
       <Route path="/problems" element={isSignedIn ?<ProblemsPage /> : <Navigate to="/" />} />
       <Route path="/problem/:id" element={isSignedIn ?<ProblemPage /> : <Navigate to="/" />} />
+      <Route path="/session/:id" element={isSignedIn ?<SessionPage /> : <Navigate to="/" />} />
+
     </Routes>
 
     <Toaster/>
