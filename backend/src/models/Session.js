87b6f392692df5a0be4dc03e145b09961644941f
TrunalAvhad <trunalrock2005@gmail.com>
2026-01-15 12:06:35 +0530

    dashboard create and backend session authentication fix

--- backend/src/models/Session.js
+++ backend/src/models/Session.js
@@ -1,35 +1,39 @@
-import mongoose from "mongoose";  
+import mongoose from "mongoose";
 
-const sessionSchema = new mongoose.Schema({
+const sessionSchema = new mongoose.Schema(
+  {
     problem: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     difficulty: {
-        type: String,
-        enum : ["easy", "medium", "hard"],
-        required: true,
+      type: String,
+      enum: ["easy", "medium", "hard"],
+      required: true,
     },
-    host:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
+    host: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    participant:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        default: null,
+    participant: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
     },
-    status:{
-        type: String,
-        enum : ["active", "completed"],
-        default: "active",
+    status: {
+      type: String,
+      enum: ["active", "completed"],
+      default: "active",
     },
-    callId:{
-        type: String,
-        default: "",
+    // stream video call ID
+    callId: {
+      type: String,
+      default: "",
     },
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
 const Session = mongoose.model("Session", sessionSchema);
 
